65a0a48f6de681ae41061209e61e86303da0dcd3
pennacap <64080101+pennacap@users.noreply.github.com>
2020-09-09 14:45:21 +0530

    Uploading files

---
+++ precache-manifest.e8fd2ec6fa42eeb03be48f44775c287a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9d4f74f95ebc8685d35",
+    "url": "/static/js/app.dd32caef.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "98a4f522733f6045472b",
+    "url": "/static/js/2.a357c0f4.chunk.js"
+  },
+  {
+    "revision": "d8eaa99a6c3def96c8d7fd98367f648d",
+    "url": "/static/media/search.a264e97b.png"
+  },
+  {
+    "revision": "115d9a31c8f60e517340fdbe831779a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/static/js/2.a357c0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "23cf81870fd7f24599616b2b77a29531",
+    "url": "/manifest.json"
+  }
+];
